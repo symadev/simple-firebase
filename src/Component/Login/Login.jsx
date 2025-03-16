3e33846cc841467a66970e604f6b6a269e94936c
syma sultana <symasultana2002@gmail.com>
2025-03-16 15:11:17 +0600

    display user info

--- src/Component/Login/Login.jsx
+++ src/Component/Login/Login.jsx
@@ -1,20 +1,26 @@
 // import { GoogleAuthProvider } from "firebase/auth/web-extension";
-import { GoogleAuthProvider } from "firebase/auth";
+import { GoogleAuthProvider, signOut } from "firebase/auth";
 import auth from "../../Firebase/Firebase";
 import { signInWithPopup } from "firebase/auth";
+import { useState } from "react";
 
 
 const Login = () => {
 
+
+    const [user,setUser] = useState([]);
     const provider = new GoogleAuthProvider()
     // এটি Firebase Authentication-এ Google Sign-In (গুগল লগইন) ব্যবহারের জন্য প্রয়োজন হয়।
     const handleGoogleSignIn = () => {
         signInWithPopup(auth, provider)
+        //now its a callback like fetch
             .then((result) => {
-                console.log(result);
+                console.log(result.user);
+                setUser(result.user);
             })
             .catch((error) => {
                 console.error("Error signing in:", error);
+                setUser(null)
             });
     };
 
@@ -31,14 +37,55 @@ const Login = () => {
 
 // 4️⃣ ইউজারের তথ্য ব্যবস্থাপনা
 // ➡️ result অবজেক্ট থেকে ইউজারের ইমেইল, নাম, প্রোফাইল ছবি ইত্যাদি এক্সেস করা যায়।
+
+
+
+const handleGoogleSignout = () => {
+    signOut(auth)
+    //now its a callback like fetch
+        .then(() => {
+            console.log('sign out done');
+            setUser(null)
+            //sign out is opposite
+           
+        })
+        .catch((error) => {
+            console.error( error);
+           
+        });
+};
     
 
 
-
+//if user exists ? signin:signout
 
 return (
         <div>
-            <button onClick= {handleGoogleSignIn}>Login With Google</button>
+            {/* <button onClick= {handleGoogleSignIn}>Login With Google</button>
+            <br />
+            <button onClick= {handleGoogleSignout}>Sign Out</button> */}
+
+
+            {
+                user ?
+                <button onClick= {handleGoogleSignout}>Sign Out</button>:
+                //user jdi thake taile signout
+                 <button onClick= {handleGoogleSignIn}>Login</button>
+              
+            }
+
+
+
+            {user && <div>
+            <h4>{user.displayName}</h4>
+            <p>{user.email}</p>
+            <img src={user.photoURL}alt="" />
+ 
+            </div>
+            }
+          
+
+
         </div>
     );
 };
